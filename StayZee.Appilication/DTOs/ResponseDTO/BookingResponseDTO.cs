cc8b52ec4f512e3ee0f7c830f6d2e6aa9a320996
Gishanan <gishananp@gmail.com>
2025-12-10 13:44:40 +0530

    change

--- StayZee.Appilication/DTOs/ResponseDTO/BookingResponseDTO.cs
+++ StayZee.Appilication/DTOs/ResponseDTO/BookingResponseDTO.cs
@@ -6,11 +6,11 @@
         public Guid CustomerId { get; set; }
         public Guid HomeId { get; set; }
         public string? HomeName { get; set; }
+        public List<string>? HomeImages { get; set; } // new
         public DateTime CheckInDate { get; set; }
         public DateTime CheckOutDate { get; set; }
         public decimal TotalPrice { get; set; }
         public string? BookingStatus { get; set; }
-      
         public DateTime CreatedAt { get; set; }
         public List<Guid>? SharedCustomerIds { get; set; }
         public List<string>? SharedCustomerEmails { get; set; }
